01fbe44a77029b8e94e48b2ce53e15fc1f2525e3
Mridul Ghanshala <mghansha@uwaterloo.ca>
2019-08-04 17:19:44 -0400

    Tested run on carnegie mellon writes to db

--- Helper.py
+++ Helper.py
@@ -7,6 +7,9 @@ import Searching
 import config
 
 def grabSite(url):
+    for i in range(3):
+        Searching.SEARCH_COUNT[Searching.THREADS[-1]] += 1
+        print(url)
     try:
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         return requests.get(url, timeout=7, headers=headers)
--- Searching.py
+++ Searching.py
@@ -7,10 +7,16 @@ import threading
 import json
 DB = json.load(open("db.json"))
 ALL = []
+THREADS = []
+SEARCH_COUNT = {}
 KEYWORDS = ["fall", "spring", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"]
 
 def search_all(thread,filter_by=None):
+    THREADS.append(thread)
+    if THREADS[-1] not in SEARCH_COUNT:
+        SEARCH_COUNT[THREADS[-1]] = 0
     Search(thread)
+    x = json.load(open("db.json"))[thread.partition(".com/")[2].partition("/")[0]]
 
 class Search(object):
     def __init__(self, urlVal):
@@ -20,17 +26,20 @@ class Search(object):
         self.pages = Helper.get_page_count(self.main_url)
         print(self.pages)
         self.all_threads = []
-        for i in range(1, self.pages+1):
+        threads=list()
+        for i in range(1, 21):
             for v in Helper.get_all_uni_threads(self.main_url + "//p{}".format(i)):
                 self.all_threads.append(v)
-        threads = [threading.Thread(target=Helper.extract_from_thread_url(), args=(self.thread, ar,)) for ar in self.all_threads]
+        for ar in self.all_threads:
+            x = threading.Thread(target=Helper.extract_from_thread_url, args=(self.thread_name, ar,))
+            threads.append(x)
         for thread in threads:
             thread.start()
         for thread in threads:
             thread.join()
         for val in ALL:
             print(val)
-        with open('all.json', 'w') as outfile:
+        with open('db.json', 'w') as outfile:
             json.dump(DB, outfile, indent=4)
 
 
