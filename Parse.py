82adacb846342f8252616634da5ce4c93d4bbf51
Mridul Ghanshala <mghansha@uwaterloo.ca>
2019-08-04 18:47:28 -0400

    fixed error

--- Parse.py
+++ Parse.py
@@ -5,7 +5,7 @@ def parse_html(value):
     htmlString = value['comment']
     page = bs4.BeautifulSoup(htmlString, 'lxml')
     info['username'] = page.select(".Username")[0].getText()
-    info['totalPosts'] = page.select("b")[0].getText()
+    info['totalPosts'] = page.find(lambda tag: tag.name == 'span' and tag['class'] == ['MItem', 'CountDiscussions']).getText().partition(" ")[0]
     info['profilePic'] = str(page.select(".ProfilePhotoMedium")[0]).partition('src="')[2].partition('"')[0]
     info['dtString'] = str(page.select("time")[0]).partition('datetime="')[2].partition('"')[0].partition('T')[0]
     info['content'] = page.select(".userContent")[0]
